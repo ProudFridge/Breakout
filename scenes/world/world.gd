93d5b5fb692e1bf5b293042fa79cb49a9370d51a
ProudFridge <proudfridge@gmail.com>
2026-06-09 22:26:05 -0400

    Make world boundary resize automatically to fit the viewport's size

---
+++ scenes/world/world.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var left_wall: CollisionShape2D = $WorldBoundary/Walls/LeftWall
+@onready var right_wall: CollisionShape2D = $WorldBoundary/Walls/RightWall
+@onready var bottom_wall: CollisionShape2D = $WorldBoundary/Walls/BottomWall
+@onready var top_wall: CollisionShape2D = $WorldBoundary/Walls/TopWall
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Set up the world boundaries otbe add the screen edges
+	# Should change later for custom level sizes
+	var viewportSize: Vector2 = get_viewport().size
+	left_wall.position = Vector2(0,0)
+	right_wall.position = Vector2(viewportSize.x, 0)
+	bottom_wall.position = Vector2(0, viewportSize.y)
+	top_wall.position = Vector2(0,0)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
