2e9f29bbda93d1cfaa8ad7c8f9ee3457c74655ac
ProudFridge <proudfridge@gmail.com>
2026-06-10 18:28:47 -0400

    Make the generate grid panel foldable

--- scenes/ball/ball.gd
+++ scenes/ball/ball.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-const SPEED: float = 300.0
+@export var SPEED: float = 300.0
+@export var damage: float = 1
 var initial_vector: Vector2 = Vector2(0,1)
 
 func _ready() -> void:
@@ -14,4 +15,4 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.bounce(collision.get_normal())
 		
 		if collision.get_collider() is Block:
-			collision.get_collider().take_damage(1)
+			collision.get_collider().take_damage(damage)
