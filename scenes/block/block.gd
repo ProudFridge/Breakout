51f1dedc809ec8bb8df0fb4d7790409a07c37d01
ProudFridge <proudfridge@gmail.com>
2026-06-11 20:51:54 -0400

    Make the color of the blocks become more red as they take damage

--- scenes/block/block.gd
+++ scenes/block/block.gd
@@ -2,12 +2,15 @@ extends CharacterBody2D
 class_name Block
 
 @export var health: float = 2
+var initialHealth: float
 
 @onready var label: Label = $Label
+@onready var mesh: MeshInstance2D = $MeshInstance2D
 
 func _ready() -> void:
+	initialHealth = health
 	BlockManager.add_block(self)
-	label.text = str(int(round(health)))
+	update_health_label()
 
 # Decreases the block's health
 func take_damage(damage: float) -> void:
@@ -15,8 +18,11 @@ func take_damage(damage: float) -> void:
 	if health == 0:
 		BlockManager.remove_block(self)
 		delete()
-	label.text = str(int(round(health)))
-
+	update_health_label()	
+	#mesh.modulate.r = move_toward(mesh.modulate.r, 0, health / initialHealth) 
+	mesh.modulate = mesh.modulate.lerp(Color(1,0,0), (initialHealth - health) / initialHealth)
+	print((initialHealth- health) / initialHealth)
+	
 # Sets the block's size
 func set_size(newSize: Vector2) -> void:
 	$CollisionShape2D.shape.size = newSize
@@ -29,3 +35,6 @@ func get_size() -> Vector2:
 # Deletes the block
 func delete() -> void:
 	queue_free()
+	
+func update_health_label() -> void:
+	label.text = str(int(round(health)))
