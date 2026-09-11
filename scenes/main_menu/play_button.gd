f0082c48613b7f9d4325e862c2709798be9ed8ba
ProudFridge <proudfridge@gmail.com>
2026-09-10 22:56:42 -0400

    Remove HorizontalTransitionRect scene, fix bug where TransitionRect.new() would create invisible ColorRects

--- scenes/main_menu/play_button.gd
+++ scenes/main_menu/play_button.gd
@@ -6,9 +6,12 @@ func _ready() -> void:
 	self.pressed.connect(_on_pressed)
 
 func _on_pressed() -> void:
-	var transitionRect: Resource = load("res://scenes/transition_rects/horizontal_transition_rect.tscn")
-	var instance: Node = transitionRect.instantiate()
-	instance.transitionTime = 1
-	instance.startTransitionDone.connect(SwitcherSingleton.switch_from_to.bind("res://scenes/arena/arena.tscn"))
-	get_tree().get_root().add_child(instance)
+	var transitionRect: TransitionRect = TransitionRect.new()
+	transitionRect.transitionTime = 1
+	transitionRect.set_shader(TransitionRect.TRANSITION_SHADER.get("HORIZONTAL"))
+	transitionRect.startTransitionDone.connect(SwitcherSingleton.switch_from_to.bind("res://scenes/arena/arena.tscn"))
 	
+	get_tree().get_root().add_child(transitionRect)
+	
+	# Uncomment this line and comment the above lines to disable transitions
+	#SwitcherSingleton.switch_from_to("res://scenes/arena/arena.tscn")
